34ac02479d568a60770df3b1ffb9f3069587f534
U-Jutapat-PC\Jutapat <Jutapat@Jutapat-PC.(none)>
2013-03-01 18:33:10 +0700

    fixed #4328 composer save data

--- reservarea/grails-app/composers/reservarea/MaintainComposer.groovy
+++ reservarea/grails-app/composers/reservarea/MaintainComposer.groovy
@@ -9,5 +9,23 @@ class MaintainComposer extends GrailsComposer {
 
     def afterCompose = { window ->
         // initialize components here
+         $('#confrim').on('click',{
+
+           if($('#x1')[0].getText()=="" || $('#x2')[0].getText()==""  || $('#x3')[0].getText()=="" || $('#x4')[0].getText()==""){
+                 alert('please input data')
+          }else{
+
+
+       new Maintenance(
+        	annu:$('#x1').getText(),
+        	area:$('#x3').getText(),
+        	mess:$('#x2').getText(),
+        	date:$('#x4').val()
+         
+      	).save()
+
+      	alert('successfully, keep it 100%')
+
+      	})
     }
 }
