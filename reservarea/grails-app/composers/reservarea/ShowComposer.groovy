e167be2e967622c5be8f590b7685c468430e5fa6
anurart <ikungcpe@gmail.com>
2013-03-03 23:38:42 +0700

    fixed #4361 composer show

--- reservarea/grails-app/composers/reservarea/ShowComposer.groovy
+++ reservarea/grails-app/composers/reservarea/ShowComposer.groovy
@@ -10,6 +10,35 @@ class ShowComposer extends GrailsComposer {
 
     def afterCompose = { window ->
 
+    	$('#btnSavedata').on('click',{
+
+    	//	def e = session["login_name"]
+	 
+		 new Register(
+			name:        $('#name').text(),
+        	lastname:      $('#lastname').text(),
+            age:            $('#age').text(),
+            phone:          $('#phone').text(),
+        	email:             $('#email').text(),
+			address:         $('#address').text(),
+        	birthday:      $('#birthday').text(),
+        	//login: new Login(username : e.username,password: e.password)
+
+      	).save()
+		alert('Editdata  Complete')
+		Executions.sendRedirect("/showdata.zul")
+
+		})
+
+		$('#btnCleardata').on('click',{
+	 		$('#name')[0].text = ''
+        	$('#lastname')[0].text =''
+            $('#age')[0].text =''
+            $('#phone')[0].text =''
+        	$('#email')[0].text =''
+			$('#address')[0].text =''
+        	$('#birthday')[0].text =''
+      })
 
 
 
